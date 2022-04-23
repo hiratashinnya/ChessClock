a15262f71bc1a849438cc42486eddc67aab9aaff
Shinya Hirata <hirashin0527@gmail.com>
2022-04-23 10:53:56 +0900

    ロジック部分の実装
    PlayersTimerの実装
    ChessClockのフィールド・コンストラクタを実装

---
+++ src/ChessClock.vb
@@ -0,0 +1,72 @@
+﻿Public Class ChessClock
+    Public players As List(Of PlayersTimer)
+
+    ''' <summary>
+    ''' 秒読みありか
+    ''' </summary>
+    Private _EnableByo_yomi As Boolean
+    Public Property EnableByo_yomi() As Boolean
+        Get
+            Return _EnableByo_yomi
+        End Get
+        Set(ByVal value As Boolean)
+            _EnableByo_yomi = value
+        End Set
+    End Property
+
+    ''' <summary>
+    ''' 現在の手番のプレイヤーのインデックス
+    ''' </summary>
+    Private _CurrentPlayerID As Integer
+    Public Property CurrentPlayerID() As Integer
+        Get
+            Return _CurrentPlayerID
+        End Get
+        Set(ByVal value As Integer)
+            _CurrentPlayerID = value
+        End Set
+    End Property
+
+    ''' <summary>
+    ''' ハンデありか
+    ''' </summary>
+    Private _HandicapAvailable As Boolean
+    Public Property HandicapAvailable() As String
+        Get
+            Return _HandicapAvailable
+        End Get
+        Set(ByVal value As String)
+            _HandicapAvailable = value
+        End Set
+    End Property
+
+    ''' <summary>
+    ''' 持ち時間更新イベント
+    ''' </summary>
+    ''' <param name="sender"></param>
+    ''' <param name="e"></param>
+    Public Event UpdatedHeldTime(ByVal sender As Object, ByVal e As EventArgs)
+
+    ''' <summary>
+    ''' 秒読み時間更新イベント
+    ''' </summary>
+    ''' <param name="sender"></param>
+    ''' <param name="e"></param>
+    Public Event UpdatedByo_yomiTime(ByVal sender As Object, ByVal e As EventArgs)
+
+    ''' <summary>
+    ''' 時間切れイベント
+    ''' </summary>
+    ''' <param name="sender"></param>
+    ''' <param name="e"></param>
+    Public Event TimeUp(ByVal sender As Object, ByVal e As EventArgs)
+
+
+    Public Sub New(ByVal playerNum As Integer)
+        players = New List(Of PlayersTimer)(playerNum)
+        _EnableByo_yomi = False
+        _CurrentPlayerID = 0
+        _HandicapAvailable = False
+    End Sub
+
+End Class
---
+++ src/PlayersTimer.vb
@@ -0,0 +1,77 @@
+﻿Public Class PlayersTimer
+    Private _Name As String
+    Public Property Name() As String
+        Get
+            Return _Name
+        End Get
+        Set(ByVal value As String)
+            _Name = value
+        End Set
+    End Property
+
+    Private _TotalHeldTime As Integer
+    Public Property TotalHeldTime() As Integer
+        Get
+            Return _TotalHeldTime
+        End Get
+        Set(ByVal value As Integer)
+            _TotalHeldTime = value
+        End Set
+    End Property
+
+    Private _TotalByo_yomiTime As Integer
+    Public Property TotalByo_yomiTime() As Integer
+        Get
+            Return _TotalByo_yomiTime
+        End Get
+        Set(ByVal value As Integer)
+            _TotalByo_yomiTime = value
+        End Set
+    End Property
+
+    Private _RemainHeldTime As Integer
+    Public Property RemainHeldTime() As Integer
+        Get
+            Return _RemainHeldTime
+        End Get
+        Set(ByVal value As Integer)
+            _RemainHeldTime = value
+        End Set
+    End Property
+
+    Private _RemainByo_yomiTime As Integer
+    Public Property RemainByo_yomiTime() As Integer
+        Get
+            Return _RemainByo_yomiTime
+        End Get
+        Set(ByVal value As Integer)
+            _RemainByo_yomiTime = value
+        End Set
+    End Property
+
+    Private WithEvents _OwnTimer As Timer
+    Public ReadOnly Property OwnTimer() As Timer
+        Get
+            Return _OwnTimer
+        End Get
+    End Property
+
+    Public Sub New(ByVal name As String, ByVal heldTime As Integer, ByVal countdownTime As Integer)
+        _Name = name
+
+        _TotalHeldTime = heldTime
+        _RemainHeldTime = heldTime
+
+        _TotalByo_yomiTime = countdownTime
+        _RemainByo_yomiTime = countdownTime
+
+        _OwnTimer = New Timer
+    End Sub
+
+    Private Sub RaiseTickerEvent() Handles _OwnTimer.Tick
+        RaiseEvent TickOwnTimer(Me, Nothing)
+    End Sub
+
+    Public Shared Event TickOwnTimer(ByVal sender As Object, ByVal e As EventArgs)
+
+End Class
