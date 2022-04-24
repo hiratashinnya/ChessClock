4ae7f0525a2dccedeedfdccb6d62fb49442e921d
Shinya Hirata <hirashin0527@gmail.com>
2022-04-24 13:04:26 +0900

    ロジック部分の実装
    【PlayersTimer】
    タイマー更新イベントを非静的に変更
    秒読み中判定フラグの追加
    変数名の修正漏れを修正
    パラメータ変更・タイマー操作・リセット・残り時間デクリメントメソッドの追加

--- src/PlayersTimer.vb
+++ src/PlayersTimer.vb
@@ -49,6 +49,16 @@
         End Set
     End Property
 
+    Private _DuringByo_yomi As Boolean
+    Public Property DuringByo_yomi() As Boolean
+        Get
+            Return _DuringByo_yomi
+        End Get
+        Set(ByVal value As Boolean)
+            _DuringByo_yomi = value
+        End Set
+    End Property
+
     Private WithEvents _OwnTimer As Timer
     Public ReadOnly Property OwnTimer() As Timer
         Get
@@ -56,14 +66,30 @@
         End Get
     End Property
 
-    Public Sub New(ByVal name As String, ByVal heldTime As Integer, ByVal countdownTime As Integer)
+    Public Sub New(ByVal name As String, ByVal heldTime As Integer, ByVal byo_yomiTime As Integer)
         _Name = name
 
         _TotalHeldTime = heldTime
         _RemainHeldTime = heldTime
 
-        _TotalByo_yomiTime = countdownTime
-        _RemainByo_yomiTime = countdownTime
+        _TotalByo_yomiTime = byo_yomiTime
+        _RemainByo_yomiTime = byo_yomiTime
+
+        _DuringByo_yomi = False
+
+        _OwnTimer = New Timer
+    End Sub
+
+    Public Sub ChangePlayerData(ByVal name As String, ByVal heldTime As Integer, ByVal byo_yomiTime As Integer)
+        _Name = name
+
+        _TotalHeldTime = heldTime
+        _RemainHeldTime = heldTime
+
+        _TotalByo_yomiTime = byo_yomiTime
+        _RemainByo_yomiTime = byo_yomiTime
+
+        _DuringByo_yomi = False
 
         _OwnTimer = New Timer
     End Sub
@@ -72,6 +98,51 @@
         RaiseEvent TickOwnTimer(Me, Nothing)
     End Sub
 
-    Public Shared Event TickOwnTimer(ByVal sender As Object, ByVal e As EventArgs)
+    Public Event TickOwnTimer(ByVal sender As Object, ByVal e As EventArgs)
 
+    Public Sub Reset()
+        StopTimer()
+        ResetHeldTime()
+        ResetByo_yomiTime()
+        DuringByo_yomi = False
+    End Sub
+    Public Sub ResetByo_yomiTime()
+        RemainByo_yomiTime = TotalByo_yomiTime
+    End Sub
+
+    Public Sub ResetHeldTime()
+        RemainHeldTime = TotalHeldTime
+    End Sub
+
+    ''' <summary>
+    ''' 持ち時間をデクリメントする。0未満になる場合は0にする。
+    ''' </summary>
+    Public Sub DecreaseHeldTime()
+        RemainHeldTime = RemainHeldTime - 1
+        If RemainHeldTime < 0 Then
+            RemainHeldTime = 0
+        Else
+            ' Do nothing
+        End If
+    End Sub
+
+    ''' <summary>
+    ''' 秒読み時間をデクリメントする。0未満になる場合は0にする。
+    ''' </summary>
+    Public Sub DecreaseByo_yomiTime()
+        RemainByo_yomiTime = RemainByo_yomiTime - 1
+        If RemainByo_yomiTime <= 0 Then
+            RemainByo_yomiTime = 0
+        Else
+            ' Do nothing
+        End If
+    End Sub
+
+    Public Sub StartTimer()
+        OwnTimer.Start()
+    End Sub
+
+    Public Sub StopTimer()
+        OwnTimer.Stop()
+    End Sub
 End Class
